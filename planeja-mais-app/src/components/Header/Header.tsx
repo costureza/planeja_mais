1748841c9f38576cbac8c6cbbac97035eeae7ed9
Rosa <costurezati@gmail.com>
2026-05-06 18:54:56 -0300

    header

--- planeja-mais-app/src/components/Header/Header.tsx
+++ planeja-mais-app/src/components/Header/Header.tsx
@@ -5,7 +5,18 @@ export const Header: React.FC = ()=>{
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <h1>planeja <span>+</span></h1>
+        <a href="#">
+          <h1>planeja <span>+</span></h1>
+        </a>
+        <nav>
+          <ul>
+            <li><a href="#">Meus Gastos</a></li>
+            <li><a href="#">Agenda Financeira</a></li>
+            <li><a href="#">Planejamento</a></li>
+          </ul>
+        </nav>  
+        
+        
       </div>
 
     </header>
