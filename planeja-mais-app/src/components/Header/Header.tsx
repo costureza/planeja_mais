60931e54a30ace37b466f2fcd45eeef1be5a0892
Rosa <costurezati@gmail.com>
2026-05-06 19:21:06 -0300

    .

--- planeja-mais-app/src/components/Header/Header.tsx
+++ planeja-mais-app/src/components/Header/Header.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "./style.module.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faCircleUser } from '@fortawesome/free-regular-svg-icons'; 
 
-export const Header: React.FC = ()=>{
+export const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -13,13 +15,19 @@ export const Header: React.FC = ()=>{
             <li><a href="#">Meus Gastos</a></li>
             <li><a href="#">Agenda Financeira</a></li>
             <li><a href="#">Planejamento</a></li>
+            <li>
+              <a href="#">
+                <FontAwesomeIcon icon={faCircleUser} /> 
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FontAwesomeIcon icon={faBell} />
+              </a>
+            </li>
           </ul>
         </nav>  
-        
-        
       </div>
-
     </header>
-
   );
 };
